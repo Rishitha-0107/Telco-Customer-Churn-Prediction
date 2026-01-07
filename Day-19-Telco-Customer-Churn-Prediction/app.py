df2fd3f29038156d3d8899d48b325c8fe73d6f32
Rishitha-0107 <23b81a6732@cvr.ac.in>
2026-01-07 14:07:24 +0530

    Update app.py

--- Day-19-Telco-Customer-Churn-Prediction/app.py
+++ Day-19-Telco-Customer-Churn-Prediction/app.py
@@ -1,6 +1,14 @@
 import os
-import pandas as pd
+import io
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+
 
 # -------------------------------
 # 1. Load Dataset
@@ -119,4 +127,5 @@ if st.button("Predict Churn"):
         st.error(f"⚠ Likely to Churn (Probability: {probability:.2f})")
     else:
         st.success(f"✅ Likely to Stay (Probability: {1 - probability:.2f})")
+
 
